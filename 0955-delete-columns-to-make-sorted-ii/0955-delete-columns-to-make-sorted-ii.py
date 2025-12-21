721b57dcfedfbdc79e739bc40d8f546e6d20ec11
Piyush Nitnaware <77199821+Piyush-hacker@users.noreply.github.com>
2025-12-21 23:47:50 +0530

    Time: 0 ms (100%), Space: 17.6 MB (97.44%) - LeetHub

---
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        res = 0
+        checklist = range(len(strs) - 1)
+        for j in range(len(strs[0])):
+            new_checklist = []
+            for i in checklist:
+                if strs[i][j] > strs[i+1][j]:
+                    res += 1
+                    break
+                elif strs[i][j] == strs[i+1][j]:
+                    new_checklist.append(i)
+            else:
+                if not new_checklist:
+                    return res
+                checklist = new_checklist
+        return res
